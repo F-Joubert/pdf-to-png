bd71683b3c341811973256984dedb49ee7f14dc8
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-05-26 13:17:31 +1000

    Added exception handling
    Added exception handling for pdf copy/print protect

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
-import fitz  # PyMuPDF
+import fitz
+import pikepdf
 from datetime import datetime
 from PIL import Image
 
@@ -39,6 +40,36 @@ def pdfs_to_single_png(pdf_path, output_path, dpi=300):
         if settings_dict["Log PNG"]:
             add_event_to_database_table(f"{datetime.today()}", "PNG", f"Saved combined image to {output_path}", "logs")
     except Exception as e:
-        print(f"PDF Conversion error: {e}")
+        print(f"PDF Conversion error: {e}\nAttempting to remove DRM and try again.")
         if settings_dict["Log Errors"]:
-            add_event_to_database_table(f"{datetime.today()}", "Error", f"Failed to Convert {pdf_path} - Error: {e}", "logs")
+            add_event_to_database_table(f"{datetime.today()}", "Error", fr"Failed to Convert {pdf_path} - Error: {e}", "logs")
+        try:
+            with pikepdf.open(pdf_path, allow_overwriting_input=True) as pdf:
+                print(fr"File {pdf_path} opened correctly.")
+                pdf.save(pdf_path)
+                print(fr"File {pdf_path} unlocked.")
+            all_images = []
+
+            pdf_document = fitz.open(pdf_path)
+            for page_num in range(len(pdf_document)):
+                page = pdf_document.load_page(page_num)
+                pix = page.get_pixmap(dpi=dpi)
+                img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+                all_images.append(img)
+        
+            max_width = max(image.width for image in all_images)
+            total_height = sum(image.height for image in all_images)
+        
+            combined_image = Image.new("RGB", (max_width, total_height))
+        
+            y_offset = 0
+            for image in all_images:
+                combined_image.paste(image, (0, y_offset))
+                y_offset += image.height
+        
+            combined_image.save(output_path, "PNG")
+            print(f"Saved combined image to {output_path}")
+            if settings_dict["Log PNG"]:
+                add_event_to_database_table(f"{datetime.today()}", "PNG", fr"Saved combined image to {output_path}", "logs")
+        except Exception as e:
+            print(f"PDF Conversion error: {e}\nSecond attempt without DRM. Aborting.")
--- watcher.py
+++ watcher.py
@@ -42,17 +42,17 @@ def on_created(event):
                 if os.path.isfile(abs_path):
                     os.remove(abs_path)
                     if settings_dict["Log Deletes"]:
-                        add_event_to_database_table(f"{datetime.today()}", "Delete", f"Delete source pdf: {abs_path}", "logs")
+                        add_event_to_database_table(f"{datetime.today()}", "Delete", fr"Delete source pdf: {abs_path}", "logs")
                 
     except Exception as err:
         print(f"Watcher error: {err}")
         if settings_dict["Log Errors"]:
-            add_event_to_database_table(f"{datetime.today()}", "Error", f"Failed to Convert {event.src_path} - Error: {err}", "logs")
+            add_event_to_database_table(f"{datetime.today()}", "Error", fr"Failed to Convert {event.src_path} - Error: {err}", "logs")
 
 def on_deleted(event):
     try:
         if settings_dict["Log Deletes"]:
-            print(f"{event.src_path} has been deleted.")
+            print(fr"{event.src_path} has been deleted.")
     except Exception as e:
         print(f"Monitor delete event error: {e}")
 
@@ -60,15 +60,15 @@ def on_modified(event):
     try:
         if settings_dict["Log Modifies"]:
             print(f"{event.src_path} has been modified.")
-            add_event_to_database_table(f"{datetime.today()}", "Modify", f"{event.src_path} has been modified.", "logs")
+            add_event_to_database_table(f"{datetime.today()}", "Modify", fr"{event.src_path} has been modified.", "logs")
     except Exception as e:
         print(f"Monitor modify event error: {e}")
 
 def on_moved(event):
     try:
         if settings_dict["Log Moves"]:
-            print(f"{event.src_path} has been moved to {event.dest_path}.")
-            add_event_to_database_table(f"{datetime.today()}", "Modify", f"{event.src_path} has been moved to {event.dest_path}.", "logs")
+            print(fr"{event.src_path} has been moved to {event.dest_path}.")
+            add_event_to_database_table(f"{datetime.today()}", "Modify", fr"{event.src_path} has been moved to {event.dest_path}.", "logs")
     except Exception as e:
         print(f"Monitor move event error: {e}")
 
@@ -92,7 +92,7 @@ def periodic_log_writer(interval):
         write_logs_to_text("logs")
         print(f"{datetime.today()} - Synced database to logs.txt")
 
-startup_message = f"""
+startup_message = fr"""
     PDF to PNG Converter v1.5
     -------------------------
     Monitoring {path} for pdfs
